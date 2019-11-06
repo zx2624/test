926e0d357d3ef844c6585b148ebff799d905adf8
xin.zhang <xin02.zhang@horizon.ai>
2019-11-06 12:02:17 +0800

    test.cc

--- test.cc
+++ test.cc
@@ -1,16 +1,17 @@
+#include <tf/tf.h>
 #include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <chrono>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
 #include <functional>
+#include <iomanip>
 #include <iostream>
 #include <limits>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#include <vector>
-#include <eigen3/Eigen/Core>
-#include <eigen3/Eigen/Geometry>
 #include <set>
-#include <chrono>
-#include <iomanip>
-#include <boost/algorithm/string.hpp>
+#include <vector>
 using namespace std;
 // #include
 enum HobotLabels {
@@ -31,42 +32,100 @@ enum HobotLabels {
   moving_object = 14,
   background = 15
 };
+Eigen::Matrix3d unsym_mat(Eigen::Vector3d vec) {
+  Eigen::Matrix3d mat;
+  mat << 0, -1 * vec(2), vec(1), vec(2), 0, -1 * vec(0), -1 * vec(1), vec(0), 0;
+  return mat;
+}
 int main() {
-
-  //0710
-  double yaw = 3.1415926/2 * 1.3, pitch = 0.2, roll = 0;
-  //0104
-  // double yaw = 1.4330470610715276, pitch =  -1.493158152010737, roll = 0.1380770137450042;
+  // 0710
+  double yaw = 1.586142764025011, pitch = 3.119029983253778,
+         roll = -3.129042033180273;
+  // 0104
+  // double yaw = 1.4330470610715276, pitch =  -1.493158152010737, roll =
+  // 0.1380770137450042; Eigen::Matrix3d mat =
+  //        Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX())*
+  //       Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()) *
+  //       Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ())
+  //     .matrix();
   Eigen::Matrix3d mat =
       Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ()) *
       Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()) *
       Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX()).matrix();
-    
-      				Eigen::Matrix3d delta_mat; delta_mat << 0, -1, 0, 1, 0, 0, 0, 0, 1;
-  std::cout << mat*delta_mat << "   mat " << std::endl;
+  std::cout << mat << std::endl;
+  std::cout << "======================" << std::endl;
+  Eigen::Matrix3d mat_2 =
+      Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ()) *
+      Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX()) *
+      Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()).matrix();
+  std::cout << mat_2 << std::endl;
+
+  tf::Matrix3x3 mat_tf;
+  mat_tf.setRPY(roll, pitch, yaw);
+  tf::Vector3 rpy_tf;
+  mat_tf.getRPY(rpy_tf[0], rpy_tf[1], rpy_tf[2]);
+  std::cout << rpy_tf[0] << " " << rpy_tf[1] << " " << rpy_tf[2] << " "
+            << std::endl;
+            
+  //////////////////////
+  Eigen::Quaterniond q_cam_chasis;
+  q_cam_chasis.w() = 0.5277614059740843;
+  q_cam_chasis.x() = 0.4506400033782436;
+  q_cam_chasis.y() = -0.4934785109517533;
+  q_cam_chasis.z() = 0.5242808836381074;
+  Eigen::Vector3d t_cam_chsis;
+  t_cam_chsis.x() = -0.1871581245824825;
+  t_cam_chsis.y() = 1.250596260979218;
+  t_cam_chsis.z() = -2.936984079567986;
+
+  Eigen::Quaterniond q_chasis_lidar;
+  q_chasis_lidar.x() = 0.0;
+  q_chasis_lidar.y() = 0.0;
+  q_chasis_lidar.z() = -0.00436330928475;
+  q_chasis_lidar.w() = 0.999990480721;
+  Eigen::Vector3d t_chasis_lidar;
+  t_chasis_lidar.x() = 0.755;
+  t_chasis_lidar.y() = 0.04;
+  t_chasis_lidar.z() = 1.97;
+
+  std::cout << "-----------now-----------" << std::endl;
 
 
-  Eigen::Quaterniond q_mat(mat);
-  Eigen::Quaterniond delta_q(delta_mat);
-  std::cout << (q_mat*delta_q).matrix() << std::endl;
-  // q_mat.toRotationMatrix()
+  std::cout << (q_cam_chasis * q_chasis_lidar).coeffs() << std::endl;
+  std::cout << "-----------now-----------" << std::endl;
+  std::cout << t_cam_chsis + q_cam_chasis * t_chasis_lidar << std::endl;
+  std::cout << "=============================" << std::endl;
 
-  Eigen::Quaterniond q; q = mat;
-  std::cout << "w:" << q.w() << " "
-  << "x:" << q.x() << " "
-  << "y:" << q.y() << " "
-  << "z:" << q.z() << std::endl;
-  // std::cout << mat.inverse() << std::endl
-  // << mat.inverse().eval() << std::endl;
+  Eigen::Quaterniond q_chasis_gnss;
+  q_chasis_gnss.x() = -0.0;
+  q_chasis_gnss.y() = 0.0;
+  q_chasis_gnss.z() = 0.707106781187;
+  q_chasis_gnss.w() = -0.707106781187;
+  Eigen::Vector3d t_chasis_gnss;
+  t_chasis_gnss.x() = 1.291;
+  t_chasis_gnss.y() = 0.46;
+  t_chasis_gnss.z() = 1.548;
 
-  string command = "mkdir -p bddd";
-  system(command.c_str());
-  std::string str = "a/b/cde.bag";
-  std::vector<std::string> str_vec;
-  boost::split(str_vec, str, boost::is_any_of("/."));
-  for(auto s : str_vec){
-    std::cout << s << std::endl;
-  }
-    // double t = (double)(ms.count() / 1000.0f);
-    // std::cout << t << std::endl;
+  Eigen::Quaterniond q_gnss_chasis = q_chasis_gnss.inverse();
+  Eigen::Vector3d t_gnss_chasis = -(q_gnss_chasis * t_chasis_gnss);
+
+  std::cout << (q_gnss_chasis * q_chasis_lidar).toRotationMatrix().eulerAngles(2, 1, 0) << std::endl;
+
+  std::cout << "-----------now-----------" << std::endl;
+  std::cout << t_gnss_chasis + q_gnss_chasis * t_chasis_lidar << std::endl;
+  
+
+  std::cout << "===============" << std::endl;
+  Eigen::Quaterniond qlc;
+  qlc.w() = 0.5277614059740843;
+  qlc.x() = 0.4506400033782436;
+  qlc.y() = -0.4934785109517533;
+  qlc.z() = 0.5242808836381074;
+
+  Eigen::Vector3d tlc;
+  tlc.x() = -0.1871581245824825;
+  tlc.y() = 1.250596260979218;
+  tlc.z() = -2.936984079567986;
+
+  std::cout << -(qlc.inverse() * tlc) << std::endl;
 }
